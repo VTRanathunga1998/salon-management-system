073be2f575aaf28df1a2088318310187f66f7a9f
vtranathunga98 <virajtharuka.fb@gmail.com>
2026-08-25 15:04:28 +0530

    made admin access for reports

--- app/(dashboard)/report/layout.tsx
+++ app/(dashboard)/report/layout.tsx
@@ -5,6 +5,6 @@ export default async function ReportLayout({
 }: {
   children: React.ReactNode;
 }) {
-  await requireRole(["OWNER"]);
+  await requireRole(["OWNER", "ADMIN"]);
   return <>{children}</>;
 }
